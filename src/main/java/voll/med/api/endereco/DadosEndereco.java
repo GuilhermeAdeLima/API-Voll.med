1adf1dacb7f6766922315fb4868c6c2082726fc4
RamsesXXI <ga.lima1999@hotmail.com>
2025-02-06 15:42:57 -0300

    Medico Controller, DadosCadastroMedico, DadosEndereco, Especialidade

---
+++ src/main/java/voll/med/api/endereco/DadosEndereco.java
@@ -0,0 +1,11 @@
+package voll.med.api.endereco;
+
+public record DadosEndereco
+        (String logradouro,
+         String bairro,
+         String cep,
+         String uf,
+         String complemento,
+         String numero) {
+
+}
