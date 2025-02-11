c92ed8cd781eaf1fc6073ee734773bea415b037e
RamsesXXI <ga.lima1999@hotmail.com>
2025-02-11 17:36:47 -0300

    Bean Validation Medico

--- src/main/java/voll/med/api/endereco/DadosEndereco.java
+++ src/main/java/voll/med/api/endereco/DadosEndereco.java
@@ -1,12 +1,21 @@
 package voll.med.api.endereco;
 
-public record DadosEndereco
-        (String logradouro,
-         String bairro,
-         String cep,
-         String uf,
-         String complemento,
-         String cidade,
-         String numero) {
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+
+public record DadosEndereco(
+        @NotBlank
+        String logradouro,
+        @NotBlank
+        String bairro,
+        @NotBlank
+        @Pattern(regexp = "\\d{8}")
+        String cep,
+        @NotBlank
+        String uf,
+        String complemento,
+        @NotBlank
+        String cidade,
+        String numero) {
 
 }
