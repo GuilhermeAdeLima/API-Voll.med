4f9df6aaa1c89a6d84a9cc928fcc922462123727
RamsesXXI <ga.lima1999@hotmail.com>
2025-02-13 18:36:53 -0300

    Criar pacote paciente, entity paciente, DTOs paciente, PacienteController

--- src/main/java/voll/med/api/paciente/DadosAtualizacaoPaciente.java
+++ src/main/java/voll/med/api/paciente/DadosAtualizacaoPaciente.java
@@ -1,9 +1,9 @@
-package voll.med.api.medico;
+package voll.med.api.paciente;
 
 import jakarta.validation.constraints.NotNull;
 import voll.med.api.endereco.DadosEndereco;
 
-public record DadosAtualizacaoMedico(
+public record DadosAtualizacaoPaciente(
         @NotNull
         Long id,
         String nome,
