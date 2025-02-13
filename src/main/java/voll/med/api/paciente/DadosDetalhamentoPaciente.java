e9644e0e660ab6b9cb33bb74f8382f1e66fb07c3
RamsesXXI <ga.lima1999@hotmail.com>
2025-02-13 20:01:15 -0300

    Padronizar retornos da API

---
+++ src/main/java/voll/med/api/paciente/DadosDetalhamentoPaciente.java
@@ -0,0 +1,27 @@
+package voll.med.api.paciente;
+
+import voll.med.api.endereco.Endereco;
+
+public record DadosDetalhamentoPaciente(
+        Long id,
+        String nome,
+        String email,
+        String cpf,
+        String telefone,
+        Endereco endereco) {
+
+    public DadosDetalhamentoPaciente(Paciente paciente) {
+        this(
+                paciente.getId(),
+                paciente.getNome(),
+                paciente.getEmail(),
+                paciente.getCpf(),
+                paciente.getTelefone(),
+                paciente.getEndereco());
+    }
+
+
+
+
+
+}
