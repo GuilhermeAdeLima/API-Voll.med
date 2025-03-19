be6e000dd08363028c0892856bd1ffd325b690e7
RamsesXXI <ga.lima1999@hotmail.com>
2025-03-19 17:17:38 -0300

    Adicionado SpringDoc, liberado acessos aos endpoinsts disponibilizados pelo SpringDoc, criada classe SpringDocCongifurations para configuração da interface Swagger UI

--- src/main/java/voll/med/api/controller/ConsultaController.java
+++ src/main/java/voll/med/api/controller/ConsultaController.java
@@ -1,5 +1,6 @@
 package voll.med.api.controller;
 
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -12,6 +13,7 @@ import voll.med.api.domain.consulta.DadosDetalhamentoCunsulta;
 
 @RestController
 @RequestMapping("consultas")
+@SecurityRequirement(name = "bearer-key")
 public class ConsultaController {
 
     @Autowired
